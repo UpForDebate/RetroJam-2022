ce769b2672da32c4da8debea39f5a8f420f7fed4
Edgarrra <edgarr.amorim@gmail.com>
2022-03-27 00:07:33 +0000

    GrapplingHook

---
+++ Assets/Controller/Scripts/GrapplingHook.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour
+{
+
+    GameObject _player, _node;
+    float targetSpeed = 20f;
+    float currentSpeed = 0f;
+    GrapplingHook(GameObject player, GameObject node)
+    {
+        _player = player;
+        _node = node;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, 0.2f);
+        Vector3 direction = (_node.transform.position - _player.transform.position).normalized;
+        _player.GetComponent<CharacterController>().Move(direction * currentSpeed);
+    }
+}
